d827b3555cffdd9a024c08df88107ccdcc0998e1
Jonathan Houze <jonathan.houze@gmail.com>
2025-10-08 18:38:46 +0800

    fix: migration cmd

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -3,34 +3,21 @@ package main
 import (
 	"fmt"
 
-	"frisboo-bank/customers-write-service/internal/shared/app"
+	migrationCommand "frisboo-bank/pkg/migration/command"
 
-	"frisboo-bank/pkg/application/command"
 	"github.com/pterm/pterm"
 	"github.com/pterm/pterm/putils"
-	"github.com/spf13/cobra"
 )
 
-// // @contact.name frisboo
-// // @contact.email contact@frisboo-bank.com
 func main() {
-	cli := command.NewApplicationCli(&command.ApplicationCliConfig{
-		Name: "customers-service",
-		Header: func() {
-			fmt.Println("")
-			_ = pterm.DefaultBigText.WithLetters(
-				putils.LettersFromStringWithStyle("Customers", pterm.FgMagenta.ToStyle()),
-				putils.LettersFromStringWithStyle(" Service", pterm.FgLightBlue.ToStyle()),
-			).Render()
-		},
-		Description: "services to retrieve customers",
-	})
+	fmt.Println("")
+	_ = pterm.DefaultBigText.WithLetters(
+		putils.LettersFromStringWithStyle("Customers", pterm.FgMagenta.ToStyle()),
+		putils.LettersFromStringWithStyle(" Service", pterm.FgLightBlue.ToStyle()),
+	).Render()
 
-	if err := cli.Execute(func(cmd *cobra.Command, args []string) {
-		if err := app.NewBootstrap().Run(); err != nil {
-			panic(err)
-		}
-	}); err != nil {
+	err := migrationCommand.NewMigrationCommand().Execute()
+	if err != nil {
 		panic(err)
 	}
 }
